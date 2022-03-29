ca3d37badd15b6774fa65c8580b04d9bef8dfbbb
望京彭于晏 <zhanggx@ifeng.com>
2022-03-29 14:39:46 +0800

    update

--- proxy_pool/run_dockerfile.sh
+++ proxy_pool/run_dockerfile.sh
@@ -1,5 +1,7 @@
 
 
+docker images | grep proxy_pool | awk '{print $3}' | xargs docker rmi
+
 docker build -t proxy_pool:1.0 .
 
 docker run -it --name proxy_pool --network fin-network proxy_pool:1.0
