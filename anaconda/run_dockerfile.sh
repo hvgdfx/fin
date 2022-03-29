36ff31e10b4ea391580abda27027f013eb39e87f
望京彭于晏 <zhanggx@ifeng.com>
2022-03-29 16:16:04 +0800

    update

---
+++ anaconda/run_dockerfile.sh
@@ -0,0 +1,8 @@
+
+
+
+container_name="anaconda3"
+
+docker ps -a  | grep $container_name | awk '{print $1}' | xargs docker rm -f
+
+docker run -it --name ${container_name} --network fin-network --network-alias ${container_name} continuumio/anaconda3:1.0
