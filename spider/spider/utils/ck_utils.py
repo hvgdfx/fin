2ccabc57bd0ede8b1439176193b0b806469656ed
望京彭于晏 <zhanggx@ifeng.com>
2022-03-30 09:50:19 +0800

    update

--- spider/spider/utils/ck_utils.py
+++ spider/spider/utils/ck_utils.py
@@ -5,7 +5,7 @@ class CKClient:
     def __init__(self):
         self.username = "default"
         self.passwd = "click!@#123"
-        self.client = Client(host="localhost", port="3306", user=self.username, password=self.passwd)
+        self.client = Client(host="120.132.33.146", port="3306", user=self.username, password=self.passwd)
 
     def show_databases(self):
         return self.client.execute("show databases;")
