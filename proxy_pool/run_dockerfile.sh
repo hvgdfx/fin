81fa33f89e2a5c76765efdad9a4289f8ad8fcfed
望京彭于晏 <zhanggx@ifeng.com>
2022-03-30 14:36:01 +0800

    update

--- proxy_pool/run_dockerfile.sh
+++ proxy_pool/run_dockerfile.sh
@@ -6,7 +6,11 @@ docker ps -a  | grep $container_name | awk '{print $1}' | xargs docker rm -f
 
 docker build -t ${container_name}:1.0 .
 
-docker run -itd --name ${container_name} --network fin-network --network-alias ${container_name} ${container_name}:1.0
+docker run -itd \
+  --name ${container_name} \
+  --network host \
+  --network-alias ${container_name} \
+  ${container_name}:1.0
 
 
 
--- redis-server/docker_run_redis.sh
+++ redis-server/docker_run_redis.sh
@@ -2,4 +2,9 @@
 container_name="proxy_redis"
 
 docker ps -a | grep $container_name | awk '{print $1}' | xargs docker rm -f
-docker run -itd --name $container_name --network fin-network --network-alias $container_name redis --requirepass 123456
+docker run -itd \
+  --name $container_name \
+  --network host \
+  --network-alias $container_name \
+  --requirepass 123456 \
+  redis
