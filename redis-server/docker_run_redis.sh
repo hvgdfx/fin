a32921574598a468aaa4021d7285710684f246cb
望京彭于晏 <zhanggx@ifeng.com>
2022-03-30 15:40:20 +0800

    update

--- redis-server/docker_run_redis.sh
+++ redis-server/docker_run_redis.sh
@@ -4,6 +4,6 @@ container_name="proxy_redis"
 docker ps -a | grep $container_name | awk '{print $1}' | xargs docker rm -f
 docker run -itd \
   --name $container_name \
-  --add-host host.docker.internal:host-gateway
+  --add-host host.docker.internal:host-gateway \
   redis \
   --requirepass 123456
