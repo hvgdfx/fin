8675094d0818dcd25cd40ae1f89d473aaf4b13a4
望京彭于晏 <zhanggx@ifeng.com>
2022-03-30 14:32:49 +0800

    update

--- spider/spider/request_test.py
+++ spider/spider/request_test.py
@@ -5,5 +5,5 @@ import sys
 sys.path.append("/work")
 sys.path.append("/home/prod/007/fin/spider")
 
-r = requests.get("http://localhost:5010/get")
+r = requests.get("http://proxy_pool:5010/get")
 print(r)
