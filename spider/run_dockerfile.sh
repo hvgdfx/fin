a73406f305cf05707de1601af72c84caad203536
望京彭于晏 <zhanggx@ifeng.com>
2022-03-30 14:28:04 +0800

    update

--- spider/run_dockerfile.sh
+++ spider/run_dockerfile.sh
@@ -6,7 +6,6 @@ docker ps -a  | grep $container_name | awk '{print $1}' | xargs docker rm -f
 docker build -t ${container_name}:1.0 .
 docker run -it \
   --name ${container_name} \
-  --net host \
   --network host \
   ${container_name}:1.0 \
 
