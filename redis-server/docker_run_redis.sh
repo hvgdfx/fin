c8e710b94beb25566c2a94cda01c758b981562f8
望京彭于晏 <zhanggx@ifeng.com>
2022-03-31 15:09:42 +0800

    update

--- redis-server/docker_run_redis.sh
+++ redis-server/docker_run_redis.sh
@@ -4,5 +4,6 @@ container_name="proxy_redis"
 docker ps -a | grep $container_name | awk '{print $1}' | xargs docker rm -f
 docker run -itd \
   --name $container_name \
+  --net host \
   -v /home/prod/007/fin/redis-server/redis.conf:/etc/redis/redis.conf \
   redis
