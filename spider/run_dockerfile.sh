ad185d741a5d2bee89f8b75ec676011dee4396f0
望京彭于晏 <zhanggx@ifeng.com>
2022-03-31 14:42:10 +0800

    update

--- spider/run_dockerfile.sh
+++ spider/run_dockerfile.sh
@@ -6,7 +6,7 @@ docker ps -a  | grep $container_name | awk '{print $1}' | xargs docker rm -f
 docker build -t ${container_name}:1.0 .
 docker run -it \
   --name ${container_name} \
-  --add-host host.docker.internal:host-gateway \
+  --add-host host.docker.internal:172.17.0.1 \
   ${container_name}:1.0 \
 
 
