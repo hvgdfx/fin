662689c92eb59f2c8694b7332707b6cd1fb9a237
望京彭于晏 <zhanggx@ifeng.com>
2022-03-31 20:02:24 +0800

    update

--- spider/spider/db_create/create_table.sh
+++ spider/spider/db_create/create_table.sh
@@ -3,7 +3,7 @@
 
 sql=""
 sql="${sql}create database IF NOT EXISTS stock;"
-sql="${sql}use stock;create table IF NOT EXISTS stock_list (code string, name string);"
+sql="${sql}use stock;create table IF NOT EXISTS stock_list (code string, name string) ENGINE = MergeTree;"
 
 
 clickhouse-client \
