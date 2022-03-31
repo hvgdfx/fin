54f06c37d2acf9ae69441e932229059b0167e0f2
望京彭于晏 <zhanggx@ifeng.com>
2022-03-31 14:44:53 +0800

    update

--- test/test_busybox_env/docker_run_busybox.sh
+++ test/test_busybox_env/docker_run_busybox.sh
@@ -3,7 +3,7 @@ container_name="test_busybox_env"
 
 docker ps -a | grep $container_name | awk '{print $1}' | xargs docker rm -f
 
-docker run -itd \
+docker run -it \
   --name $container_name \
   --add-host host.docker.internal:172.17.0.1 \
   busybox
