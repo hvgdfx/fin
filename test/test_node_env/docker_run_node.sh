c4305fd985b8bfca16932ed893ac7843a9e6fec4
望京彭于晏 <zhanggx@ifeng.com>
2022-04-01 18:36:30 +0800

    update

--- test/test_node_env/docker_run_node.sh
+++ test/test_node_env/docker_run_node.sh
@@ -1,8 +1,13 @@
 
 container_name="test_node_env"
 
+# delete container
 docker ps -a | grep $container_name | awk '{print $1}' | xargs docker rm -f
+
+# build image
+docker build -t ${container_name}:1.0 .
+
 docker run -it \
   --name $container_name \
   --network host \
-  ${container_name}
+  ${container_name}:1.0
