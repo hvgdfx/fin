98cdaf020c81ee79f85ce87154d5bb4be9d927fe
望京彭于晏 <zhanggx@ifeng.com>
2022-04-02 18:23:19 +0800

    update

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 
+# project
 1. proxy
 2. redis
 3. spider
--- spider/spider/db_create/create_table.sh
+++ spider/spider/db_create/create_table.sh
@@ -3,15 +3,25 @@
 
 sql=""
 sql="${sql}create database IF NOT EXISTS stock;"
-sql="${sql}use stock;create table IF NOT EXISTS stock_list (
-          code String, name String, dt String
+#sql="${sql}use stock;create table IF NOT EXISTS
+#          stock_list (
+#          code String,
+#          name String,
+#          dt String
+#          ) ENGINE = MergeTree
+#          PARTITION BY dt;"
+
+sql="${sql}use stock;create table IF NOT EXISTS
+          trade_date (
+          dt String,
+          isOpen String,
+          dt String
           ) ENGINE = MergeTree
           PARTITION BY dt;"
-
 
 clickhouse-client \
   --host localhost \
   --port 9000 \
   --user default \
   --password click!@#123\
-   --multiquery -q  "${sql}"
+  --multiquery -q  "${sql}"
