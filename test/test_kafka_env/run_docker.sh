c43acb1a13702556bdc4ba99b9369ef3725934b9
zhanggx <zhanggx@ifeng.com>
2023-11-27 13:52:56 +0800

    update

--- test/test_kafka_env/run_docker.sh
+++ test/test_kafka_env/run_docker.sh
@@ -1,2 +1,10 @@
 
 
+name='test-kafka-env'
+
+docker images | grep ${name} | awk '{print $3}' | xargs docker rmi -f
+docker ps -a | grep ${name} | awk '{print $1}' | xargs docker rm -f
+
+
+docker build -t ${name}:1.0 --no-cache -f ./Dockerfile .
+docker run --name ${name} --network host ${name}:1.0
