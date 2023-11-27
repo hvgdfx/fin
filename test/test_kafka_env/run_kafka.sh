3239a1420febb67dcbd74f30b59bb78a01292ffa
zhanggx <zhanggx@ifeng.com>
2023-11-27 19:14:57 +0800

    update

--- test/test_kafka_env/run_kafka.sh
+++ test/test_kafka_env/run_kafka.sh
@@ -5,9 +5,9 @@ cd /opt/bitnami/kafka
 
 sleep 10
 
-./bin/kafka-server-start.sh ./server1.properties &
-./bin/kafka-server-start.sh ./server2.properties &
-./bin/kafka-server-start.sh ./server3.properties &
+#./bin/kafka-server-start.sh ./server1.properties &
+#./bin/kafka-server-start.sh ./server2.properties &
+#./bin/kafka-server-start.sh ./server3.properties &
 
 while true;
   do sleep 3600;
