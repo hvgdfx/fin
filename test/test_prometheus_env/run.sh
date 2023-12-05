ac5d63f77a59e9806c3b21fc9c18a39d7988c91f
zhanggx <zhanggx@ifeng.com>
2023-12-05 15:31:18 +0800

    update

--- test/test_prometheus_env/run.sh
+++ test/test_prometheus_env/run.sh
@@ -1,6 +1,5 @@
 
 
-docker run \
-  --web.listen-address=:59080 \
+docker run --name test-prometheus-env \
   -v prometheus.yml:/opt/bitnami/prometheus/conf/prometheus.yml \
   bitnami/prometheus:2.48.0
