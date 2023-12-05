d81446082178ebded4c2136ba0db590d11b1b4c8
zhanggx <zhanggx@ifeng.com>
2023-12-05 18:57:45 +0800

    update

--- spider/spider/db_create/trade_date.py
+++ spider/spider/db_create/trade_date.py
@@ -40,7 +40,7 @@ def get_all_trade_date():
 
 def insert_all_trade_date(dt):
     # file_path = "./all_trade_data.txt"
-    file_path = f"./tradeData_{dt}.txt"
+    file_path = f"./tradeDate_{dt}.txt"
     if os.path.exists(file_path):
         pass
     else:
