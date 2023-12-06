1b4a162e754c01725a2256e0e949115b7a23852a
zhanggx <zhanggx@ifeng.com>
2023-12-06 14:03:44 +0800

    update

--- redis_server/docker_run_redis.sh
+++
@@ -1,9 +0,0 @@
-
-container_name="stock_proxy_redis"
-
-docker ps -a | grep $container_name | awk '{print $1}' | xargs docker rm -f
-docker run -itd \
-  --name $container_name \
-  --net host \
-  -v /home/prod/007/fin/redis-server/redis.conf:/etc/redis/redis.conf \
-  -d redis redis-server /etc/redis/redis.conf
---
+++ redis_server/run_dockerfile.sh
@@ -0,0 +1,9 @@
+
+name="stock_proxy_redis"
+
+docker images | grep ${name} | awk '{print $3}' | xargs docker rmi -f
+docker ps -a | grep ${name} | awk '{print $1}' | xargs docker rm -f
+
+
+docker build -t ${name}:1.0 --no-cache -f ./Dockerfile .
+docker run -d --name ${name} --user root ${name}:1.0
