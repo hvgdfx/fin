09dc3735b402718b77f17e06c347c5ffd3d0b021
zhanggx <zhanggx@ifeng.com>
2023-12-06 20:13:06 +0800

    update

--- spider/spider/utils/alter.py
+++ spider/spider/utils/alter.py
@@ -15,7 +15,7 @@ def send_message(text):
                              data=json.dumps(data1).encode('utf-8'))
 
         if resp.status_code == 200:
-            print(f"alter success")
+            print(f"alter success " + resp.text)
         else:
             print(f"alter failed {resp.status_code}")
     except Exception as e:
@@ -27,7 +27,7 @@ def send_message2(text):
         data1 = {}
         data1["msgtype"] = "text"
         data1["at"]: {"isAtAll": True}
-        data1["text"] = {"content": text}
+        data1["text"] = {"content": "warning " + text}
         url = "https://oapi.dingtalk.com/robot/send?access_token=767e3d391fd9a07641f0c2d2f93dad0f3f90a8a1c0c77d4aef3ed92adb6953a8"
 
         import urllib
