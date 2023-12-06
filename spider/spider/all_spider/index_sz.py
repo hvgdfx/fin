099e8052dc9e1c405654f99c1dcac02d0230fd48
zhanggx <zhanggx@ifeng.com>
2023-12-06 15:21:44 +0800

    update

--- spider/spider/all_spider/index_sz.py
+++ spider/spider/all_spider/index_sz.py
@@ -1,6 +1,7 @@
 import requests
 from fake_useragent import UserAgent
 
+ua = UserAgent()
 
 # 1. request
 
@@ -9,19 +10,20 @@ def requset_index_sz():
     url = "http://query.sse.com.cn/commonSoaQuery.do?jsonCallBack=jsonpCallback311835&isPagination=false&sqlId=DB_SZZSLB_ZSLB&_=1701760270888"
     # 2. headers
 
-    proxy_json = requests.get("http://stock_proxy:5010/get")
-    if not proxy_json["http"]:
+    proxy_json = requests.get("http://stock_proxy:5010/get").json()
+    if proxy_json["https"]:
         print("没有http代理可用")
         return
     proxies = {
-        "http": proxy_json["http"]
+        "http": proxy_json["proxys"]
     }
 
     headers = {
-        "user-agent": UserAgent(),
+        "user-agent": ua.random,
         "proxies": proxies
     }
-
+    print(f"headers: {headers}")
+    
     # 3. response
     resp = requests.get(url=url, headers=headers)
     if resp.status_code == 200:
