a5efb9563f9e96bd314078b1b84689ae8e3ad8d3
zhanggx <zhanggx@ifeng.com>
2023-12-06 15:09:48 +0800

    update

--- spider/spider/all_spider/index_sz.py
+++ spider/spider/all_spider/index_sz.py
@@ -8,14 +8,18 @@ def requset_index_sz():
     # 1. url
     url = "http://query.sse.com.cn/commonSoaQuery.do?jsonCallBack=jsonpCallback311835&isPagination=false&sqlId=DB_SZZSLB_ZSLB&_=1701760270888"
     # 2. headers
-    requests.get("")
 
+    proxy_json = requests.get("http://stock_proxy:5010/get")
+    if not proxy_json["http"]:
+        print("没有http代理可用")
+        return
     proxies = {
-        "http", requests.get("http://stock_proxy:5010/get")
+        "http": proxy_json["http"]
     }
 
     headers = {
         "user-agent": UserAgent(),
+        "proxies": proxies
     }
 
     # 3. response
