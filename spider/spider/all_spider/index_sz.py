d554970719615116a574061c023fcd60d1b6ec00
zhanggx <zhanggx@ifeng.com>
2023-12-06 16:40:42 +0800

    update

--- spider/spider/all_spider/index_sz.py
+++ spider/spider/all_spider/index_sz.py
@@ -5,71 +5,87 @@ ua = UserAgent()
 
 
 # 1. request
-
 def requset_index_sz():
-    # 1. url
-    url = "http://query.sse.com.cn/commonSoaQuery.do?jsonCallBack=jsonpCallback311835&isPagination=false&sqlId=DB_SZZSLB_ZSLB&_=1701760270888"
-    # 2. headers
-
-    proxy_json = requests.get("http://stock_proxy:5010/get").json()
-    print(f"proxy_json: {proxy_json}")
-    if proxy_json["https"]:
-        print("没有http代理可用")
-        return
-
-    proxies = {
-        "http": f"http://{proxy_json['proxy']}"
-    }
-
-    headers = {
-        "user-agent": ua.random,
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Host": "query.sse.com.cn",
-        "Pragma": "no-cache",
-        "Referer": "http://www.sse.com.cn/",
-    }
-    print(f"headers: {headers}")
-
-    # 3. response
-    resp = requests.get(url=url, headers=headers, proxies=proxies)
     try_count = 0
     while True:
+        # 1. url
+        url = "http://query.sse.com.cn/commonSoaQuery.do?jsonCallBack=jsonpCallback311835&isPagination=false&sqlId=DB_SZZSLB_ZSLB&_=1701760270888"
+        # 2. headers
+
+        proxy_json = requests.get("http://stock_proxy:5010/get").json()
+        print(f"proxy_json: {proxy_json}")
+        if proxy_json["https"]:
+            print("没有http代理可用")
+            return
+
+        proxies = {
+            "http": f"http://{proxy_json['proxy']}"
+        }
+
+        headers = {
+            "user-agent": ua.random,
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Host": "query.sse.com.cn",
+            "Pragma": "no-cache",
+            "Referer": "http://www.sse.com.cn/",
+        }
+        print(f"headers: {headers}")
+        print(f"proxies: {proxies}")
+
+        # 3. response
+
         try_count += 1
+        resp = requests.get(url=url, headers=headers, proxies=proxies)
         if try_count > 3:
             break
         if resp.status_code == 200:
             return resp
+        else:
+            print(f"resp {try_count} status code {resp.status_code}")
     return None
-
 
 
 # 2. parse data
 def parse_response(resp):
-    result = {}
-    try:
-        resp = resp.json()
-        result = resp
-    except Exception as e:
-        print(e)
-        return result
+    # 1. check data
+    # 2. parse data
+    data = {}
+    if resp is None:
+        return None
+    else:
+        try:
+            data = resp.text
+        except Exception as e:
+            print(e)
+            print(f"parse response {data}")
+    return data
 
-    # 正常
 
-    return result
+def check_data(resp) -> bool:
+    return False
+
+
+def parse_data(resp):
+    pass
 
 
 # 3. insert data
+def insert_data(data):
+    pass
+
 
 def run():
     resp = requset_index_sz()
-
     if resp is None:
         return
-    
+    print(f"-----------------------------------------------")
+
     data = parse_response(resp)
+    print(type(data))
+    print(f"-----------------------------------------------")
     print(list(data.keys()))
 
 
