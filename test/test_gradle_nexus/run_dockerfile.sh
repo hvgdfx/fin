8b66121f70363faf9bbb37c26d73a6a1f7633e47
zhanggx <zhanggx@ifeng.com>
2024-05-08 15:05:58 +0800

    update

---
+++ test/test_gradle_nexus/run_dockerfile.sh
@@ -0,0 +1,10 @@
+
+
+name='test-nexus-env'
+
+docker images | grep ${name} | awk '{print $3}' | xargs docker rmi -f
+docker ps -a | grep ${name} | awk '{print $1}' | xargs docker rm -f
+
+
+docker build -t ${name}:1.0 --no-cache -f ./Dockerfile .
+docker run -d --name ${name} --user root --network host -p 8091:8091 ${name}:1.0
