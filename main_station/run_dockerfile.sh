83397e5879a06776ce8e048e09b6aa36f1c7ae5f
zhanggx <zhanggx@ifeng.com>
2024-06-19 14:30:26 +0800

    udpate

--- main_station/run_dockerfile.sh
+++ main_station/run_dockerfile.sh
@@ -1,11 +1,16 @@
 
 
 docker pull ccr.ccs.tencentyun.com/library/nginx:latest
-name='test-nginx-env'
+name='dalinghua-nginx'
 
 docker images | grep ${name} | awk '{print $3}' | xargs docker rmi -f
 docker ps -a | grep ${name} | awk '{print $1}' | xargs docker rm -f
 
 
 docker build -t ${name}:1.0 --no-cache -f ./Dockerfile .
-docker run -d --name ${name} --user root --network host ${name}:1.0
+docker run -d --name ${name} \
+    -p 443:443 \
+    -v /data/wuchang/dalinghua.cn.key:/etc/nginx/dalinghua.cn.key \
+    -v /data/wuchang/dalinghua.cn.pem:/etc/nginx/dalinghua.cn.pem \
+    --user root \
+    --network host ${name}:1.0
