4b66120f642177b7e348950cd9682939bfd12243
zhanggx <zhanggx@ifeng.com>
2024-06-27 14:06:35 +0800

    update

--- ton/run.sh
+++ ton/run.sh
@@ -6,4 +6,4 @@ docker images | grep ${name} | awk '{print $3}' | xargs docker rmi -f
 docker ps -a | grep ${name} | awk '{print $1}' | xargs docker rm -f
 
 docker build -t ${name}:1.0 --no-cache -f ./Dockerfile .
-docker run --name ${name} --user root ${name}:1.0
+docker run -d --name ${name} --user root ${name}:1.0
